661d045cf091c61923de67e5cf393a3fed21941a
coreydd2002 <coreydd2002@gmail.com>
2026-03-16 20:07:40 -0600

    initial

---
+++ backend/Bookstore.API/Controllers/BookstoreController.cs
@@ -0,0 +1,28 @@
+﻿using Bookstore.API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BookstoreController : ControllerBase
+    {
+        private BookDbContext _bookcontext;
+        public BookstoreController(BookDbContext temp) => _bookcontext = temp;
+
+        [HttpGet("AllBooks")]
+        public IEnumerable<Book> GetBooks()
+        {
+            return _bookcontext.Books.ToList();
+        }
+
+        [HttpGet("FictionalBooks")]
+
+        public IEnumerable<Book> GetFictionalBooks()
+        {
+            var fictional = _bookcontext.Books.Where(p => p.Classification == "Fiction");
+            return fictional.ToList();
+        }
+    }
+}
